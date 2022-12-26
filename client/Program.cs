a118263943e5bb2f3fa7ac87cc760aae5dfb499f
Brad <bmasciotra@my.bcit.ca>
2022-12-25 19:18:37 -0800

    working on the cli

--- client/Program.cs
+++ client/Program.cs
@@ -23,13 +23,43 @@ class Program
     }
 
 
+    private static void Upload_Photo()
+    {
+        Console.WriteLine("Please input the path to the image you wish to upload:");
+
+        string image_path = Console.ReadLine();
+
+
+
+    }
+
+
     private static bool Launch_Application()
     {
         bool continue_program = true;
-        string input = Console.ReadLine();
-        Console.WriteLine($"input: {input}");
-        string input_regex = @"^[1-4]$";
-        Match m = Regex.
+        string input          = Console.ReadLine();
+        string input_regex    = @"^[1-4]$"; // match for only 1 number from 1 - 4
+        bool m                = Regex.IsMatch(input, input_regex);
+
+        if (m)
+        {
+            Console.WriteLine("Here");
+            Console.WriteLine(int.Parse(input));
+            switch (int.Parse(input))
+            {
+                case 1:
+                    Upload_Photo();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
+
+        Console.WriteLine($"match: {m}");
 
 
         return continue_program;
